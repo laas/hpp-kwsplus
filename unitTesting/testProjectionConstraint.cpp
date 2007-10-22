1a740b3939a41092a97b09508b0f0e4c09f23e3c
florent <florent@poullenc.laas.fr>
2007-10-22 12:08:43 +0200

    Added a class to test whether a configuration constraint is continuous and is a projection.

--- unitTesting/testProjectionConstraint.cpp
+++ unitTesting/testProjectionConstraint.cpp
@@ -1,92 +1,4 @@
-#include <iostream>
-#include "KineoWorks2/kwsConstraint.h"
-#include "KineoWorks2/kwsDiffusionShooter.h"
-#include "KineoWorks2/kwsDevice.h"
-
-
-#if TOYOTA
-
-std::ostream& operator<<(std::ostream& os, const CkwsConfig& config);
-
-std::ostream& operator<<(std::ostream& os, const CkwsConfig& config)
-{
-  std::vector<double> vec;
-  config.getDofValues(vec);
-  unsigned int dim = config.size();
-  
-  for (unsigned int i=0; i < dim; i++) {
-    os << vec[i] << " ";
-  }
-  return os;
-}
-
-#else
-#include "kwsioConfig.h"
-#endif
-
-
-/**
-   \brief Test whether a constraint is a projection over a manifold
-
-   To use this object, you need to
-   \li define a device,
-   \li define a configuration constraint (CkwsConstraint) for this device ,
-   \li add this constraint to the device,
-   \li create a CtestProjectionConstraint with input this constraint.
-
-   Then, you can call methods 
-   \li CtestProjectionConstraint::continuity to test the continuity of the constraint and
-   \li CtestProjectionConstraint::projection to test that the constraint is a projection over a manifold.
-*/
-
-class CtestProjectionConstraint
-{
-public:
-  /**
-     \brief Constructor taking a reference to a configuration constraint.
-  */
-  CtestProjectionConstraint(const CkwsConstraintShPtr& inConstraint);
-
-  /**
-     \brief test continuity
-     \param inNbTest number of random configurations generated
-
-     Generate a given number of random pairs of configurations close to each other.
-     Apply the constraint to both configuration and computes the ratio:
-     distance between projections over distance between configurations.
-  */
-  bool continuity(unsigned int inNbTest);
-
-  /**
-     \brief Projection
-     \param inNbTest number of random configurations generated
-
-     Generate a given number of configurations. For each of them, applies the constraint 
-     twice and tests that second application of constraint does not change the configuration.
-  */
-  bool projection(unsigned int inNbTest);
-
-private:
-  /**
-     \brief Constraint to be tested.
-  */
-  CkwsConstraintShPtr attConstraint;
-  
-  /**
-     \brief Device associated to the constraint.
-  */
-  CkwsDeviceShPtr attDevice;
-
-  /**
-     \brief Medium configuration
-  */
-  CkwsConfig attMediumConfig;
-
-  /**
-     \brief Standard deviation 
-  */
-  double attStandardDeviation;
-};
+#include "testProjectionConstraint.h"
 
 
 CtestProjectionConstraint::CtestProjectionConstraint(const CkwsConstraintShPtr& inConstraint) :
@@ -117,7 +29,7 @@ bool CtestProjectionConstraint::continuity(unsigned int inNbTest)
     //
     // Generate second configuration close to first one.
     //
-    if (CkwsDiffusionShooter::gaussianShoot(config2, attStandardDeviation) == KD_OK) {
+    if (shootAtDistance(config2, attStandardDeviation) == KD_OK) {
       
       //
       // Set device in first configuration: the constraint is applied
@@ -201,4 +113,25 @@ bool CtestProjectionConstraint::projection(unsigned int inNbTest)
     }
   }
   return result;
+}
+
+
+
+ktStatus CtestProjectionConstraint::shootAtDistance(CkwsConfig& inOutConfig, double inStandardDeviation)
+{
+  CkwsConfig randomConfig(attDevice); 
+  randomConfig.randomize();
+
+  double dist = inOutConfig.distanceTo(randomConfig);
+
+  if (dist <=0 ) {
+    std::cerr << "CtestProjectionConstraint::shootAtDistance: negative distance" << std::endl;
+    return KD_ERROR;
+  }
+
+  double coef = inStandardDeviation/dist;
+  for (unsigned int iDof=0; iDof < attDevice->countDofs(); iDof++) {
+    inOutConfig.dofValue(iDof, (1-coef)*inOutConfig.dofValue(iDof) + coef*randomConfig.dofValue(iDof));
+  }
+  return KD_OK;
 }
